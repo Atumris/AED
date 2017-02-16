004085645e88a56d0f72d2ba3b7d79d54b56e1c9
Joey <super55@live.nl>
2017-02-16 14:37:50 +0100

    SequentialSearch almost done

--- Tester/MainDLL/H04/SequenTialSearch.cs
+++ Tester/MainDLL/H04/SequenTialSearch.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MainDLL.H02;
 
 namespace MainDLL.H04
 {
     class SequenTialSearch <T> where T : IComparable
     {
-        Array = new ArrayLis
+        ArrayList<T> asdf = new ArrayList<T>();
         static bool SeqSearch(T[] arr, int sValue)
         {
             for (int index = 0; index < arr.Length - 1; index++)
