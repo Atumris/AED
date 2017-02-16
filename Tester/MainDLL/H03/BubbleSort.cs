f27747d6ab0193c9f23fcbd26079be2dc33e64bc
Atumris <mirek_nalepa@live.nl>
2017-02-16 14:17:43 +0100

    Added comments

--- Tester/MainDLL/H03/BubbleSort.cs
+++ Tester/MainDLL/H03/BubbleSort.cs
@@ -10,22 +10,36 @@ namespace MainDLL.H03
     {
         private T[] array;
 
+        /// <summary>
+        /// Bubble sort initioation
+        /// </summary>
+        /// <param name="inputarray"></param>
+        /// <returns name="inputarray"></returns>
         public static T[] PerformBubbleSort(T[] inputarray)
         {
+            //int containing the lenght of inputarray
             int lenght = inputarray.Length;
+            //int to temporarily store data
             T temp = inputarray[0];
 
+            //loop trough the lenght of the array
             for (int i = 0; i < lenght; i++)
             {
+                //loop trough the array from index one
                 for (int j = i+1; j < lenght; j++)
                 {
+                    //compare inputarray element to the next element in array
                     if (inputarray[i].CompareTo(inputarray[i + 1]) > 0)
                     {
+                        //store the data in a temporary array
                         temp = inputarray[i];
+                        //change the array data to the data stored in the other array to move on.
                         inputarray[i] = inputarray[j];
+                        // change the other array to the temporary data state.
                         inputarray[j] = temp;
                     }
                 }
+              //return sorted array.
             } return inputarray; 
         }
     }
--- Tester/MainDLL/H03/SmartBubbleSort.cs
+++ Tester/MainDLL/H03/SmartBubbleSort.cs
@@ -10,26 +10,41 @@ namespace MainDLL.H03
     {
         private T[] array;
 
+        /// <summary>
+        /// Bubble smart with isSorted flag
+        /// </summary>
+        /// <param name="inputarray"></param>
+        /// <returns name ="inputarray"></returns>
         public static T[] PerformBubbleSort(T[] inputarray)
         {
+            //define local variables
             int lenght = inputarray.Length;
             T temp = inputarray[0];
             bool isSorted = false;
 
+            //loop while the list is not sorted
             while (!isSorted)
             {
+                //set the state of the local variable to true to stop the loop in case the array is already sorted.
                 isSorted = true;
+                //loop trough the array length
                 for (int i = 0; i < inputarray.Length -1; i++)
                 {
+                    //check if the inputarray is already sorted or not.
                     if (inputarray[i].CompareTo(inputarray[i + 1]) > 0)
                     {
+                        //set the temporary data.
                         temp = inputarray[i];
+                        //change the data in the inputarray to compare the data.
                         inputarray[i] = inputarray[i + 1];
+                        //set the inputarray data to the temp data to compare that.
                         inputarray[i + 1] = temp;
+                        //set the isSorted bool to false to keep itterating.
                         isSorted = false;
                     }
                 }
             }
+            //return inputarray.
             return inputarray;
         }
     }
