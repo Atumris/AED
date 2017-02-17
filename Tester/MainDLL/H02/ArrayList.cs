55ed6ec993f6d3b24c09cca81c593bf1b2e1c504
Henri1992 <henriarends@live.nl>
2017-02-17 15:34:39 +0100

    Enumerator test

--- Tester/MainDLL/H02/ArrayList.cs
+++ Tester/MainDLL/H02/ArrayList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MainDLL.H02
 {
-    public class ArrayList <T> where T : IComparable
+    public class ArrayList <T> where T : IComparable, IEnumerator
     {
         private T[] array;
         private int size;
