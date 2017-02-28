39b969641a7c54dd78f87065c7f0dbc30e02aaad
Joey <super55@live.nl>
2017-02-28 16:27:23 +0100

    SQSEARCH WERKT??

--- Tester/MainDLL/Search/SequenTialSearch.cs
+++ Tester/MainDLL/Search/SequenTialSearch.cs
@@ -4,16 +4,12 @@ namespace MainDLL.Search
 {
     class SequenTialSearch <T> where T : IComparable
     {
-        //static bool SeqSearch(ArrayList<T> arr, int sValue)
-        //{
-        //    //foreach(T value in arr)
-        //    //{
-
-        //    //}
-        //    //for (int index = 0; index < arr.Length - 1; index++)
-        //    //    if (arr[index] == sValue)
-        //    //        return true;
-        //    //return false;
-        //}
+        static bool SeqSearch(T[] arr, T sValue)
+        {
+            for (int index = 0; index < arr.Length - 1; index++)
+                if (arr[index].CompareTo(sValue) == 1)
+                    return true;
+            return false;
+        }
     }
 }
