a0b241d403840cc83e7e9321038a33e476678095
Atumris <mirek_nalepa@live.nl>
2017-03-02 12:27:11 +0100

    smart and bubble sort.
    fixed comments ,
    
    simplified code

--- Tester/MainDLL/Sort/BubbleSort.cs
+++ Tester/MainDLL/Sort/BubbleSort.cs
@@ -7,56 +7,35 @@ namespace MainDLL.Sort
         private T[] array;
 
         /// <summary>
-        /// Bubble sort initioation
-        /// </summary>
-        /// <param name="inputarray"></param>
-        /// <returns name="inputarray"></returns>
-        public T[] PerformBubbleSort(T[] inputarray)
-        {
-            //int to temporarily store data
-            T temp = inputarray[0];
-
-            //loop trough the lenght of the array
-            for (int i = 0; i < inputarray.Length; i++)
-            {
-                //loop trough the array from index one
-                for (int j = 0; j < inputarray.Length - 1; j++)
-                {
-                    //compare inputarray element to the next element in array
-                    if (inputarray[j].CompareTo(inputarray[i + 1]) > 0)
-                    {
-                        //store the data in a temporary array
-                        temp = inputarray[i];
-                        //change the array data to the data stored in the other array to move on.
-                        inputarray[i] = inputarray[j];
-                        // change the other array to the temporary data state.
-                        inputarray[j] = temp;
-                    }
-                }
-              //return sorted array.
-            } return inputarray; 
-        }
-
-        /// <summary>
         /// Bubblesort sorts the array with lowest value first
         /// </summary>
         /// <param name="arr"></param>
         /// <returns></returns>
         public T[] Bubble(T[] arr)
         {
+            //variable to store the lenght of the array.
             int length = arr.Length;
 
+            //Loop untill there is no more data to loop trough.
             for (int i = 0; i < length; i++)
             {
+                //Loop to get the second index. 
                 for (int j = 0; j < length - 1; j++)
                 {
+                    //Check which number is higher and replace in case the next number is lower than the first.
                     if (arr[j].CompareTo(arr[j + 1]) > 0)
                         Swap(ref arr[j], ref arr[j + 1]);
                 }
             }
+            //return the sorted array.
             return arr;
         }
-
+        
+        /// <summary>
+        /// Method to swap the indexes in the array.
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
         static void Swap(ref T lhs, ref T rhs)
         {
             var temp = lhs;
--- Tester/MainDLL/Sort/SmartBubbleSort.cs
+++ Tester/MainDLL/Sort/SmartBubbleSort.cs
@@ -7,51 +7,15 @@ namespace MainDLL.Sort
         private T[] array;
 
         /// <summary>
-        /// Bubble smart with isSorted flag
-        /// </summary>
-        /// <param name="inputarray"></param>
-        /// <returns name ="inputarray"></returns>
-        public static T[] PerformBubbleSort(T[] inputarray)
-        {
-            //define local variables
-            int lenght = inputarray.Length;
-            T temp = inputarray[0];
-            bool isSorted = false;
-
-            //loop while the list is not sorted
-            while (!isSorted)
-            {
-                //set the state of the local variable to true to stop the loop in case the array is already sorted.
-                isSorted = true;
-                //loop trough the array length
-                for (int i = 0; i < inputarray.Length - 1; i++)
-                {
-                    //check if the inputarray is already sorted or not.
-                    if (inputarray[i].CompareTo(inputarray[i + 1]) > 0)
-                    {
-                        //set the temporary data.
-                        temp = inputarray[i];
-                        //change the data in the inputarray to compare the data.
-                        inputarray[i] = inputarray[i + 1];
-                        //set the inputarray data to the temp data to compare that.
-                        inputarray[i + 1] = temp;
-                        //set the isSorted bool to false to keep itterating.
-                        isSorted = false;
-                    }
-                }
-            }
-            //return inputarray.
-            return inputarray;
-        }
-
-        /// <summary>
         /// Bubblesort sorts the array with lowest value first
         /// </summary>
         /// <param name="arr"></param>
         /// <returns></returns>
         public T[] SmartBubble(T[] arr)
         {
+            //bool to check if the array is already sorted
             bool isSorted = false;
+            //get the length of the array
             int length = arr.Length;
 
             //loop while the list is not sorted
@@ -59,20 +23,31 @@ namespace MainDLL.Sort
             {
                 //set the state of the local variable to true to stop the loop in case the array is already sorted.
                 isSorted = true;
+                //loop to get the first index
                 for (int i = 0; i < length; i++)
                 {
+                    //loop to get second index
                     for (int j = 0; j < length - 1; j++)
                     {
+                        //check if the second index is smaller than the first
                         if (arr[j].CompareTo(arr[j + 1]) > 0)
                         {
+                            //swap the indexes when the second is smaller than the first
                             Swap(ref arr[j], ref arr[j + 1]);
+                            //set the boolean to false so the loop continues untill it is sorted.
                             isSorted = false;
                         }
                     }
                 }
             }
+            //return sorted array
             return arr;
         }
+        /// <summary>
+        /// method to handle index swapping.
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
         static void Swap(ref T lhs, ref T rhs)
         {
             var temp = lhs;
