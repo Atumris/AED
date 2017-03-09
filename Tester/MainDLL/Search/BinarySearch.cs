85ff63901a547dfc8965061a54735bfef8aa3f65
Henri1992 <henriarends@live.nl>
2017-03-09 14:28:55 +0100

    Binary search first draft
    The first draft of binary search. Some tweaking needed.

--- Tester/MainDLL/Search/BinarySearch.cs
+++ Tester/MainDLL/Search/BinarySearch.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace MainDLL.Search
 {
-    class BinarySearch
+    class BinarySearch<T> where T : IComparable
     {
+        public bool BinSearch(int item, int[] collection)
+        {
+            int len = collection.Length;
+            int[] p1 = collection.Take(len / 2).ToArray();
+            int[] p2 = collection.Skip(len / 2).ToArray();
+            bool found = false;
+            while (!found)
+            {
+                if (p1.Length != 1)
+                {
+                    int p1val = Int32.Parse(p1.GetValue(p1.Length - 1).ToString());
+                    int p2val = Int32.Parse(p2.GetValue(0).ToString());
+                    if (p1val > item)
+                    {
+                        p1 = p1.Take(p1.Length / 2).ToArray();
+                        p2 = p1.Skip(len / 2).ToArray();
+                    }
+                    else if (p2val < item)
+                    {
+                        p1 = p2.Take(p2.Length / 2).ToArray();
+                        p2 = p2.Skip(len / 2).ToArray();
+                    }
+                    else if (p2val == item || p1val == item)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return found;
+                }
+            }
+            return found;
+        }
     }
 }
